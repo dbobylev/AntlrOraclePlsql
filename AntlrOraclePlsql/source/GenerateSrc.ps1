7f2591c2e52cdfe21c89f9e8d9a1fcd0d494f381
Bobylev Dmitry <netlight4me@gmail.com>
2020-06-16 22:12:17 +0300

    do not delete "antlr-4.8-complete.jar" file

--- AntlrOraclePlsql/source/GenerateSrc.ps1
+++ AntlrOraclePlsql/source/GenerateSrc.ps1
@@ -1,6 +1,9 @@
-Invoke-WebRequest https://www.antlr.org/download/antlr-4.8-complete.jar -OutFile .\antlr-4.8-complete.jar
+if(![System.IO.File]::Exists(".\antlr-4.8-complete.jar"))
+{
+  Invoke-WebRequest https://www.antlr.org/download/antlr-4.8-complete.jar -OutFile .\antlr-4.8-complete.jar
+}
 java -jar .\antlr-4.8-complete.jar -Dlanguage=CSharp -o ..\src\ -package AntlrOraclePlsql .\PlSqlLexer.g4
 java -jar .\antlr-4.8-complete.jar -Dlanguage=CSharp -o ..\src\ -package AntlrOraclePlsql -visitor .\PlSqlParser.g4
 del ..\src\*.tokens
 del ..\src\*.interp
-del *.jar
+#del *.jar
